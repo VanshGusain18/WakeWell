18095deba2ffd603aa43e0b14061c3a13a3bf451
Paridhi <goelparidhi2021@gmail.com>
2026-02-11 11:43:41 +0530

    rise4

--- WakeWell/features/Rise Rituals/RitualDetailViewController.swift
+++ WakeWell/features/Rise Rituals/RitualDetailViewController.swift
@@ -11,7 +11,9 @@ class RitualDetailViewController: UIViewController {
 
     
     @IBOutlet weak var backgroundImage: UIImageView!
-    @IBOutlet weak var popupView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descImage: UIImageView!
+    @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var descLabel: UILabel!
     
     var ritual: Ritual?
@@ -21,34 +23,29 @@ class RitualDetailViewController: UIViewController {
     }
 
     func setupUI() {
-            guard let ritual = ritual else { return }
+        guard let ritual = ritual else { return }
             
-            backgroundImage.image = UIImage(named: ritual.imagePath) // or ritual.image
-            descLabel.text = ritual.description
+        backgroundImage.image = UIImage(named: ritual.imagePath)
+        titleLabel.text = ritual.name
+        descImage.image = UIImage(named: ritual.imagePath)
+        descImage.layer.cornerRadius = 20
+        descLabel.text = ritual.description
             
-            let tap = UITapGestureRecognizer(target: self, action: #selector(dismissMe))
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissMe))
             view.addGestureRecognizer(tap)
-       // let blurEffectView.alpha = 0.7
-        }
+    }
 
         @objc func dismissMe() {
             dismiss(animated: true)
         }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        //popupView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
-        popupView.alpha = 0
-        popupView.layer.cornerRadius = 20
-        popupView.layer.masksToBounds = true
-        
        
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.6, options: .curveEaseOut, animations: {
-            self.popupView.transform = .identity
-            self.popupView.alpha = 1
         }, completion: nil)
     }
 

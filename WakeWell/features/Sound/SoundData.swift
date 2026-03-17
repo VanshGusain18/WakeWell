247ee57464371c0a9ddf42c2d92600ac3e0978e2
ZOYA <>
2026-03-17 14:40:57 +0530

    model

---
+++ WakeWell/features/Sound/SoundData.swift
@@ -0,0 +1,177 @@
+var allSounds: [Sound] = [
+   //ambience
+    Sound(
+        title: "White Noise",
+        category: .ambient,
+        duration: 600,
+        fileName: "light_music-white-noise-188847",
+        imageName: "soundsrelaxing89-cozy-7023760"
+        ),
+    Sound(
+        title: "Beats of Nature",
+        category: .ambient,
+        duration: 600,
+        fileName: "folk_acoustic-the-beat-of-nature-122841",
+        imageName: "beena777-ai-generated-9594995"
+    ),
+    Sound(
+        title: "Piano Strings",
+        category: .ambient,
+        duration: 600,
+        fileName: "good_b_music-ambient-piano-and-strings-10711",
+        imageName: "ralf1403-piano-10046998 1"
+    ),
+    Sound(
+        title: "Meditation Music",
+        category: .ambient,
+        duration: 600,
+        fileName: "grand_project-breath-of-enlightenment-meditation-music-407178",
+        imageName: "couleur-candles-9874140"
+        ),
+    Sound(
+        title: "Railway Tracks",
+        category: .ambient,
+        duration: 600,
+        fileName: "grand_project-railway-track-470218",
+        imageName: "tama66-tunnel-4427609"
+        ),
+    Sound(
+        title: "Cinematic ambient",
+        category: .ambient,
+        duration: 600,
+        fileName: "lexin_music-inspiring-cinematic-ambient-116199",
+        imageName: "foundry-shop-868071"
+        ),
+    Sound(
+        title: "Coorporate World",
+        category: .ambient,
+        duration: 600,
+        fileName: "sound4stock-soft-background-corporate-music-472861",
+        imageName: "rus-burkhanov-building-4803602"
+        ),
+    Sound(
+        title: "Vibing relaxing music",
+        category: .ambient,
+        duration: 600,
+        fileName: "vibehorn-relax-lofi-beat-461489",
+        imageName: "iffany-ai-generated-9380435"
+        ),
+    //Nature
+    Sound(
+        title: "Saving Nature",
+        category: .nature,
+        duration: 600,
+        fileName: "emmraan-saving-nature-218413",
+        imageName: "sonyuser-river-5765785"
+        ),
+    Sound(
+        title: "Melody",
+        category: .nature,
+        duration: 600,
+        fileName: "good_b_music-melody-of-nature-main-6672",
+        imageName: "flower"
+        ),
+    Sound(
+            title: "Flowing Water",
+            category: .nature,
+            duration: 600,
+            fileName: "natureseye-the-old-water-mill-meditation-8005",
+            imageName: "pexels-sea-1850228"
+            ),
+    Sound(
+        title: "Xylophone beats",
+        category: .nature,
+        duration: 600,
+        fileName: "mandakimdk-xylophone-and-forest-307174",
+        imageName: "ralf1403-sheet-music-8463988 1"
+        ),
+    Sound(
+        title: "Forest",
+        category: .nature,
+        duration: 600,
+        fileName: "rikardfrizz-the-enchanted-forest-252441",
+        imageName: "beena777-ai-generated-9594995"
+        ),
+    Sound(
+        title: "Soundscape",
+        category: .nature,
+        duration: 600,
+        fileName: "surprising_media-ambient-nature-soundscape-344565",
+        imageName: "pinecone"
+        ),
+    Sound(
+        title: "Spiritual Forest",
+        category: .nature,
+        duration: 600,
+        fileName: "sonican-spiritual-nature-364866",
+        imageName: "stills_by_suki-banff-8329971"
+        ),
+    Sound(
+            title: "Wind And Bells",
+            category: .nature,
+            duration: 600,
+            fileName: "meditativetiger-sleep-inducing-tibetan-bells-388638",
+            imageName: "chrisart16-bell-4401148"
+            ),
+   
+    //Weather
+    Sound(
+        title: "Rain",
+        category: .weather,
+        duration: 600,
+        fileName: "desifreemusic-relaxing-sleep-music-with-soft-ambient-rain-369762",
+        imageName: "hungvina1986-season-4559795"
+        ),
+    Sound(
+        title: "Wind",
+        category: .weather,
+        duration: 600,
+        fileName: "grand_project-zen-wind-411951",
+        imageName: "manfredrichter-windmill-7963566"
+        ),
+    
+Sound(
+    title: "Brighter Tomorrow",
+    category: .weather,
+    duration: 600,
+    fileName: "49534488-a-brighter-tomorrow-333965",
+    imageName: "heungsoon-cherry-blossom-7889717"
+    ),
+    Sound(
+        title: "Partly cloudy skies",
+        category: .weather,
+        duration: 600,
+        fileName: "49534488-partly-cloudy-skies-return-333957",
+        imageName: "wpquadrosemolduras-rainbow-5039388"
+        ),
+    Sound(
+        title: "Rain cocoon",
+        category: .weather,
+        duration: 600,
+        fileName: "book_of_life-rain-cocoon-316178",
+        imageName: "chamelete-caterpillar-2482317"
+        ),
+    Sound(
+        title: "Weather Melody",
+        category: .weather,
+        duration: 600,
+        fileName: "musicword-weather-melody-182846",
+        imageName: "congvuphotographer-landscape-5104510"
+        ),
+    Sound(
+        title: "Spring",
+        category: .weather,
+        duration: 600,
+        fileName: "musicword-spring-weather-312701",
+        imageName: "chiemseherin-meadow-5229169"
+        ),
+    Sound(
+        title: "Autumn",
+        category: .weather,
+        duration: 600,
+        fileName: "musicword-autumn-weather-236905",
+        imageName: "heungsoon-maple-leaves-2789234"
+        )
+     
+    
+]

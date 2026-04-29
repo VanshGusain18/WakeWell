fc4b0fb60b848b1f66f944ad5412e53e6bca8ca3
Vansh Gusain <vanshgusain18@gmail.com>
2026-04-29 10:16:58 +0530

    backend

---
+++ WakeWell Watch App Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WakeWell Watch App Watch App
+//
+//  Created by geu on 29/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WakeWell Watch App Watch App/WakeWell_Watch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakeWell_Watch_AppApp.swift
+//  WakeWell Watch App Watch App
+//
+//  Created by geu on 29/04/26.
+//
+
+import SwiftUI
+
+@main
+struct WakeWell_Watch_App_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
